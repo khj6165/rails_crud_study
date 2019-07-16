bf4dd18107ca78ef1fd772d56b3bb4acdec88beb
root <khj6165@gmail.com>
2019-07-16 19:18:50 +0000

    add all

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+	
+	def new
+	end
+	
+	def create
+		post = Post.new
+		post.title = params[:title]
+		post.content = params[:content]
+		post.save
+		
+		redirect_to "/"
+	end
+	
+	def delete
+		post = Post.find(params[:id])
+		post.destroy
+		
+		redirect_to "/"
+	end
+	
+	def edit
+		@post = Post.find(param[:id])
+	end
+	
+	def update
+		@post = Post.find(param[:id])
+		post.title = params[:title]
+		post.content = params[:content]
+		post.save
+		
+		redirect_to "/"
+	end
+end
